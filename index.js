647b4bf73e4612378606208830dfd97f6a747717
norman <41760554+n0rmancodes@users.noreply.github.com>
2020-06-12 17:14:09 -0400

    start ouo.io / ouo.press decryption

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const n = require("needle");
 const h = require("http");
 const u = require("url");
 const ue = require("expand-url");
-console.log("")
+const fD = require('form-data');
+const c = require('cheerio');
+console.log("STARTING SERVER...");
 h.createServer(onRequest).listen(3000);
+console.log("STARTED!")
 
-function onRequest(request,response) {
+function onRequest(request,res) {
 	var ul = u.parse(request.url,true);
 	var l = ul.query.url;
 	
@@ -15,21 +18,95 @@ function onRequest(request,response) {
 			"err":"noUrlFound",
 			"version":"1.0"
 		})
-		response.writeHead(404, {
+		res.writeHead(404, {
 			"Content-Type": "application/json",
 			"Access-Control-Allow-Origin": "*"
 		});
-		response.end(d);
-	} else if (l.includes("goo.gl")){
+		res.end(d);
+	} else if (l.includes("bit.ly") | l.includes("goo.gl")) {
 		ue.expand(l, function (err, longUrl) {
-			var d = JSON.stringify({
-				"link":longUrl
-			})
-			response.writeHead(404, {
-				"Content-Type": "application/json",
+			if (longUrl && !err) {
+				var d = JSON.stringify({
+					"link":longUrl,
+					"resolvedUsing":"expand-url"
+				})
+				res.writeHead(200, {
+					"Content-Type": "application/json",
+					"Access-Control-Allow-Origin": "*"
+				});
+				res.end(d);
+			} else {
+				n("https://apimon.de/redirect/" + l, function (error, response) {
+					var j = response.body;
+					if (j.valid == true) {
+						var d = JSON.stringify({
+							"link":j.destination,
+							"reslovedUsing":"apimon-fallback"
+						})
+						res.writeHead(200, {
+							"Content-Type": "application/json",
+							"Access-Control-Allow-Origin": "*"
+						});
+					} else {
+						var d = JSON.stringify({
+							"err":"noLink"
+						})
+						res.writeHead(404, {
+							"Content-Type": "application/json",
+							"Access-Control-Allow-Origin": "*"
+						});
+						res.end(d);
+					}
+				})
+			}
+		})
+	} else if (l.includes("ouo.io") | l.includes("ouo.press")) {
+		n(l, function(error, response) {
+			var h = response.body;
+			res.writeHead(200, {
 				"Access-Control-Allow-Origin": "*"
-			});
-			response.end(d);
+			})
+			res.end(h);
+			var $ = c.load(h);
+			$('input').each(function () {
+				var a = $(this)
+				if (a.attr('name') == "_token") {
+					var k = a.val()
+					
+					var hdrs = {
+						"Host": "www.google.com",
+						"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0",
+						"Accept": "*/*",
+						"Accept-Language": "en-US,en;q=0.5",
+						"Accept-Encoding": "gzip, deflate, br",
+						"Content-Type": "application/x-protobuffer",
+						"Content-Length": "4378",
+						"Origin": "https://www.google.com",
+						"Connection": "keep-alive",
+						"DNT": "1",
+						"TE": "Trailers"
+					}
+					n.post("https://www.google.com/recaptcha/api2/reload?k=6Lcr1ncUAAAAAH3cghg6cOTPGARa8adOf-y9zv2x", hdrs, function (error, response) {
+						
+					})
+				}
+			})
+			
+		})
+	} else {
+		n("https://apimon.de/redirect/" + l, function (error, response) {
+			var j = response.body;
+			if (j.valid == true) {
+				var d = JSON.stringify({
+					"link":j.destination,
+					"resolvedUsing":"apimon-generic"
+				})
+				res.writeHead(200, {
+					"Content-Type": "application/json",
+					"Access-Control-Allow-Origin": "*"
+				});
+				res.end(d);
+			}
 		})
 	}
 }
