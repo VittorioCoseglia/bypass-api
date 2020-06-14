43ed606908cf6eeab581598b98e8aa75ad934e15
norman <41760554+n0rmancodes@users.noreply.github.com>
2020-06-14 15:03:42 -0400

    started linkvertise script

--- index.js
+++ index.js
@@ -41,17 +41,9 @@ function onRequest(request,res) {
 					if (j.valid == true) {
 						var d = JSON.stringify({
 							"link":j.destination,
-							"reslovedUsing":"apimon-fallback"
+							"resolvedUsing":"apimon-generic"
 						})
 						res.writeHead(200, {
-							"Content-Type": "application/json",
-							"Access-Control-Allow-Origin": "*"
-						});
-					} else {
-						var d = JSON.stringify({
-							"err":"noLink"
-						})
-						res.writeHead(404, {
 							"Content-Type": "application/json",
 							"Access-Control-Allow-Origin": "*"
 						});
@@ -61,6 +53,7 @@ function onRequest(request,res) {
 			}
 		})
 	} else if (l.includes("ouo.io") | l.includes("ouo.press")) {
+		//not completed
 		n(l, function(error, response) {
 			var h = response.body;
 			res.writeHead(200, {
@@ -93,6 +86,46 @@ function onRequest(request,res) {
 			})
 			
 		})
+	} else if (l.includes("linkvertise.com") | l.includes("linkvertise.net") | l.includes("direct-link.net") | l.includes("file-link.net") | l.includes("up-to-down.net") | l.includes("link-to.net")) {
+		let k = {timestamp:new Date().getTime(),random:"375123"}
+			console.log(" - linkvertise script - ")
+			var ur = u.parse(l,true);
+			var p = ur.pathname;
+			var a = "https://linkvertise.net/api/v1/redirect/link/static" + p;
+			console.log("-> sending 1st request");
+			n(a, function(error, response) {
+				console.log("-> 1st response recieved")
+				var r = response.body;
+				k.link_id = r.data.link.id;
+				var hdrs = {
+					"Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+					"Accept-Encoding":"gzip, deflate, br",
+					"Accept-Language":"en-US,en;q=0.5",
+					"Connection":"keep-alive",
+					"DNT":"1",
+					"Host":"linkvertise.net",
+					"Upgrade-Insecure-Requests":"1",
+					"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0"
+				}
+				var b = "https://linkvertise.net/api/v1/redirect/link" + p + "/target?serial=" + Buffer.from(JSON.stringify(k)).toString("base64");
+				console.log(b);
+				n("post", b, hdrs, function (error, response) {
+					console.log("-> got second response");
+					var r = response.body;
+					if (r.data) {
+						console.log("-> scraped destination, sent ")
+						var d = JSON.stringify({
+							"link":r.data.target,
+							"resolvedUsing":"linkvertise-script"
+						})
+						res.end(d)
+						console.log("-> sent server response");
+					} else {
+						res.end(r)
+						console.log("-> sent server response");
+					}
+				})
+			})
 	} else {
 		n("https://apimon.de/redirect/" + l, function (error, response) {
 			var j = response.body;
@@ -109,4 +142,14 @@ function onRequest(request,res) {
 			}
 		})
 	}
+}
+
+
+function gbVar(oUrl) {
+	n("https://apimon.de/redirect/" + oUrl, function (error, response) {
+		var j = response.body;
+		if (j.valid == true) {
+			return j.destination;
+		}
+	})
 }
